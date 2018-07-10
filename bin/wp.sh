3c137eaf316d50b4976d84f4c8a2242a5236d37c
Jostein Gogstad <jostein.gogstad@tapad.com>
2018-07-10 14:09:21 +0200

    Updated GCP utilities

---
+++ bin/wp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+function error {
+  echo "$*" >&2 && exit 1
+}
+
+if [[ -n "$1" ]]; then
+    aic "$1"
+fi
+
+function first_free_port {
+    for i in {0..9}; do
+        port="808$i"
+        if ! lsof -nP -i4"TCP:$port" > /dev/null 2>&1; then
+            echo $port
+            return 0
+        else
+            echo "Port $port busy, checking $((port + 1))..." >&2
+        fi
+    done
+    error "No free ports found"
+}
+
+port=$(first_free_port)
+
+echo "Found free $port, opening tunnel..."
+kubectl port-forward "$(kubectl get pods | awk '/airflow-webserver/ {print $1}')" "$port:8080"
