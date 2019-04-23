f73cd6175078a6741de6357765831621f85a03e8
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-04-23 13:23:13 +0200

    Add function lsd and format gitconfig

--- .zsh_config/jgogstad.zsh
+++ .zsh_config/jgogstad.zsh
@@ -1,5 +1,6 @@
 alias vim=nvim
 alias grep="grep --color=always"
+function lsd { exa --color=never -d $PWD/$1}
 
 # Make Ctrl+U behave like in bash (remove from cursor until beginning of prompt)
 bindkey \^U backward-kill-line
