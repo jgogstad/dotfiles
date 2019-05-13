7c4ad1f2e284ef0e055a5b5203936d5d74fcab23
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-05-13 10:05:54 +0200

    Remove ZSH history, add omz key bindings, remove custom ^U binding

--- .zsh_config/jgogstad.zsh
+++ .zsh_config/jgogstad.zsh
@@ -7,7 +7,7 @@ alias k=kubectl
 function lsd { exa --color=never -d $PWD/$1}
 
 # Make Ctrl+U behave like in bash (remove from cursor until beginning of prompt)
-bindkey \^U backward-kill-line
+#bindkey \^U backward-kill-line
 
 ## Enable sharing of history across multiple sessions
 setopt inc_append_history
