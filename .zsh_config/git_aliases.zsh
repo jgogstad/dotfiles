c53992751997142e88f0a33ba14d37bbd4ee0f71
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-06-03 08:09:03 +0200

    Add gblo

--- .zsh_config/git_aliases.zsh
+++ .zsh_config/git_aliases.zsh
@@ -2,6 +2,12 @@ alias glol='git log --graph --pretty='\''%Cred%h%Creset %C(yellow)%G?%Creset -%C
 alias grhs='git reset --soft'
 alias grhss='git reset --soft HEAD^1'
 
+function gblo {
+  git for-each-ref --sort=committerdate refs/heads/ --format="%(committerdate:short)|%(refname:short)|%(committername)" | while IFS='|' read date branch name; do 
+    printf "%s %-40s %s\n" "$date" "$branch" "$name"
+  done
+}
+
 function grbo {
   git rebase -i origin/$(git_current_branch) # Function from OMZ
 }
@@ -9,3 +15,4 @@ function grbo {
 function gcot {
   git checkout --track origin/${1##origin/}
 }
+
