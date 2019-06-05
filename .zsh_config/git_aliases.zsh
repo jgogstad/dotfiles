07b57264bc6518689df922cfbae28e7653002e57
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-06-05 11:36:47 +0200

    New definition for gblo
    Add git.zsh from oh-my-zsh

--- .zsh_config/git_aliases.zsh
+++ .zsh_config/git_aliases.zsh
@@ -2,9 +2,15 @@ alias glol='git log --graph --pretty='\''%Cred%h%Creset %C(yellow)%G?%Creset -%C
 alias grhs='git reset --soft'
 alias grhss='git reset --soft HEAD^1'
 
-function gblo {
+function gbll {
   git for-each-ref --sort=committerdate refs/heads/ --format="%(committerdate:short)|%(refname:short)|%(committername)" | while IFS='|' read date branch name; do 
     printf "%s %-40s %s\n" "$date" "$branch" "$name"
+  done
+}
+
+function gblo {
+  git for-each-ref --sort=committerdate refs/remotes/origin --format="%(committerdate:short)|%(refname:short)|%(committername)" | while IFS='|' read date branch name; do 
+    printf "%s %-40s %s\n" "$date" "${branch##origin/}" "$name"
   done
 }
 
