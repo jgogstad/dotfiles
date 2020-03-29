f0b5619f57da49123d336cbfb6825708ce2abfc8
Jostein Gogstad <jostein.gogstad@gmail.com>
2020-03-29 12:25:37 +0200

    Add ruby support

--- .zsh_config/macos.zsh
+++ .zsh_config/macos.zsh
@@ -29,3 +29,12 @@ if [ -f "$HOME/opt/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/opt/google-cl
 
 # The next line enables shell command completion for gcloud.
 if [ -f "$HOME/opt/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/opt/google-cloud-sdk/completion.zsh.inc"; fi
+
+# Use ruby version installed with homebrew if present. Installing gems is not possible
+# with the version that ships with macOS since it lives in /Library
+if [[ -d /usr/local/opt/ruby/bin ]]; then
+  export PATH="/usr/local/opt/ruby/bin:$PATH"
+  export LDFLAGS="-L/usr/local/opt/ruby/lib"
+  export CPPFLAGS="-I/usr/local/opt/ruby/include"
+  export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
+fi
