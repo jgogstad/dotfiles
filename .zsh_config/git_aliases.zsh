f6c9dcc1807072e77608df184bbd3de7780b6c91
Jostein Gogstad <jostein.gogstad@gmail.com>
2020-05-05 15:45:26 +0200

    Dotfiles update

--- .zsh_config/git_aliases.zsh
+++ .zsh_config/git_aliases.zsh
@@ -1,15 +1,15 @@
 # Compatibility with oh-my-zsh git plugin
 if ! [[ ${+commands[git_current_branch]} ]]; then 
   git_current_branch () {
-  	local ref
-  	ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
-  	local ret=$?
-  	if [[ $ret != 0 ]]
-  	then
-  		[[ $ret == 128 ]] && return
-  		ref=$(command git rev-parse --short HEAD 2> /dev/null)  || return
-  	fi
-  	echo ${ref#refs/heads/}
+    local ref
+    ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
+    local ret=$?
+    if [[ $ret != 0 ]]
+    then
+      [[ $ret == 128 ]] && return
+      ref=$(command git rev-parse --short HEAD 2> /dev/null)  || return
+    fi
+    echo ${ref#refs/heads/}
   }
 fi
 
--- .zsh_config/macos.zsh
+++ .zsh_config/macos.zsh
@@ -3,17 +3,16 @@ alias grep="grep --color=always"
 alias kctx=kubectx
 alias kc=kubectx
 alias k=kubectl
-alias v=vault
 alias top='top -o cpu'
-alias sbt=/usr/local/bin/sbtx
 
 function lsd { exa --color=never -d $PWD/$1}
 
 # -X: Don't clear terminal after less finish
 # -R: Display raw color codes
 # -F: Exit if the entire file can be displayed
-export LESS="-XRF"
+export LESS="-RF"
 
+# Exclude yubikey secrets and kubectx context assignments from history
 HISTORY_EXCLUDE_PATTERN="^ykchalresp|^kc .*=\.|$HISTORY_EXCLUDE_PATTERN"
 
 # Force locale
@@ -30,11 +29,13 @@ if [ -f "$HOME/opt/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/opt/google-cl
 # The next line enables shell command completion for gcloud.
 if [ -f "$HOME/opt/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/opt/google-cloud-sdk/completion.zsh.inc"; fi
 
-# Use ruby version installed with homebrew if present. Installing gems is not possible
-# with the version that ships with macOS since it lives in /Library
-if [[ -d /usr/local/opt/ruby/bin ]]; then
-  export PATH="/usr/local/opt/ruby/bin:$PATH"
-  export LDFLAGS="-L/usr/local/opt/ruby/lib"
-  export CPPFLAGS="-I/usr/local/opt/ruby/include"
-  export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
-fi
+[ -s "/Users/jostein.gogstad/.jabba/jabba.sh" ] && source "/Users/jostein.gogstad/.jabba/jabba.sh"
+
+# Don't use outdated (Mohave) version of OpenSSL with ruby
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1) --with-readline-dir=$(brew --prefix readline)"
+
+# rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export LDFLAGS="-L/usr/local/opt/libffi/lib"
+export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig"
