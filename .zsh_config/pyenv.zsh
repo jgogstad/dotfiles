371b0eaba66ea5e5205b4354262ffb169306bfcb
Jostein Gogstad <jostein.gogstad@gmail.com>
2020-09-29 12:34:53 +0200

    Pyenv doesn't modify zshrc

--- .zsh_config/pyenv.zsh
+++ .zsh_config/pyenv.zsh
@@ -1,5 +1,5 @@
-echo 'export PYENV_ROOT="/usr/local/opt/pyenv"' >> ~/.zshrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+export PYENV_ROOT="/usr/local/opt/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
